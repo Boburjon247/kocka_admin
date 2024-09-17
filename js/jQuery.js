ad0e50ad986480ba09264f1a79246e2670149a8d
Boburjon <143814695+Boburjon247@users.noreply.github.com>
2024-09-17 10:16:04 +0500

    koica-yoqlama

---
+++ js/jQuery.js
@@ -0,0 +1,10 @@
+$(".js-example-tokenizer").select2({
+    tags: true,
+    tokenSeparators: [',', ' '],
+    multiple:true,
+})
+$(".js-example-tokenizer1").select2({
+    tags: true,
+    tokenSeparators: [',', ' '],
+    multiple:true,
+})
---
+++ js/javascript.js
@@ -0,0 +1,112 @@
+const mainItems = document.querySelectorAll('.main-items');
+const homeMineBlokItem = document.querySelectorAll('.home-mine-blok-item');
+const loading = document.querySelectorAll('.loading')
+
+
+function hideTab(params) {
+    homeMineBlokItem.forEach((items, index) => {
+        items.classList.remove('active');
+    });
+    mainItems.forEach((items, index) => {
+        items.classList.remove('active');
+    });
+    loading.forEach((item,index) => {
+        item.classList.remove('active');
+    })
+}
+function showTab(index) {
+    homeMineBlokItem[index].classList.add('active');
+    mainItems[index].classList.add('active');
+    loading[index].classList.add('active');
+}
+hideTab();
+showTab(0)
+
+mainItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        localStorage.setItem('blok-item', index);
+        hideTab();
+        showTab(index);        
+    })
+    if (parseInt(localStorage.getItem('blok-item')) == `${index}`) {
+        hideTab();
+        showTab(index);
+    }
+})
+setInterval(() => {
+    loading.forEach((item,index) => {
+        item.classList.remove('active');
+    })
+}, 2000);
+
+
+const newyersbtnadd = document.querySelector('.newyersbtnadd');
+const madalWinndowNewYer = document.querySelector('.madal-winndow-new-yer1');
+const exitMadalWindow = document.querySelector('.exit-madal-window')
+newyersbtnadd.addEventListener('click', () =>{
+    madalWinndowNewYer.classList.add('active');
+});
+exitMadalWindow.addEventListener('click', ()=>{
+    madalWinndowNewYer.classList.remove('active'); 
+});
+
+
+const madalwinndownewyer = document.querySelector('.madal-winndow-edit-yer');
+const editNewYer = document.querySelectorAll('.edit_new-yer');
+const exitmadalwindowedit = document.querySelector('.exit-madal-window-edit');
+
+exitmadalwindowedit.addEventListener('click',()=>{
+    madalwinndownewyer.classList.remove('active');
+});
+editNewYer.forEach((item,index)=>{
+    item.addEventListener('click', ()=>{
+        madalwinndownewyer.classList.add('active');
+    });
+});
+
+
+function madalExit(item) {
+    item.classList.remove('active');
+}
+function madalOpen(item) {
+    item.classList.add('active');
+}
+
+document.querySelector('.exitMadalGuruh').addEventListener('click',()=>{
+    madalExit(document.querySelector('.madalGuruh'));
+});
+document.querySelector('.guruhAdd').addEventListener('click', ()=>{
+    madalOpen(document.querySelector('.madalGuruh'));
+})
+
+document.querySelectorAll('.editGuruhName').forEach((item,index) =>{
+    item.addEventListener('click', () =>{
+        madalOpen(document.querySelector('.madalGuruhEdit'))
+    });
+});
+document.querySelector('.exitMadalGuruhedit').addEventListener('click', () =>{
+    madalExit(document.querySelector('.madalGuruhEdit'))
+});
+
+
+document.querySelectorAll('.editStudentAbout').forEach((item,index) =>{
+    item.addEventListener('click', ()=>{
+        madalOpen(document.querySelector('.madalStudent'))
+    });
+});
+document.querySelector('.madalStudentEditexit').addEventListener('click', ()=>{
+    madalExit(document.querySelector('.madalStudent'))
+});
+
+const madalTeacherEdit = document.querySelector('.madalTeacherEdit');
+const editTeacherAbout = document.querySelectorAll('.editTeacherAbout');
+const madalTeacherEditexit = document.querySelector('.madalTeacherEditexit');
+
+editTeacherAbout.forEach((item,index) =>{
+    item.addEventListener('click', () =>{
+        madalOpen(madalTeacherEdit)
+    });
+});
+madalTeacherEditexit.addEventListener('click', () =>{
+    madalExit(madalTeacherEdit);
+});
